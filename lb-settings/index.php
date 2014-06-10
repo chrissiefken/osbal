d8234e3fd7e01b029bb3e8150bebac9e2a386cc7
Chris Siefken <siefkencp@gmail.com>
2014-06-10 01:55:57 -0400

    Added some UI love to the services page - still a long way to go.

--- lb-settings/index.php
+++ lb-settings/index.php
@@ -4,22 +4,25 @@ include $_SERVER['DOCUMENT_ROOT'] . '/lib/header.php';
 <h2>Online Services:</h2>
 <div class="list-group">
   <a href="#" class="list-group-item">
-    <h4 class="list-group-item-heading">List group item heading</h4>
-    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <h4 class="list-group-item-heading"><span class="glyphicon glyphicon-pencil"></span> 10.0.0.102</h4>
+    <p class="list-group-item-text">site.example.com</p>
   </a>
   <a href="#" class="list-group-item">
-    <h4 class="list-group-item-heading">List group item heading</h4>
-    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <h4 class="list-group-item-heading"><span class="glyphicon glyphicon-pencil"></span> 10.0.0.101</h4>
+    <p class="list-group-item-text">api.example.com</p>
   </a>
+  <a href="#" class="list-group-item">
+    <h4 class="list-group-item-heading"><span class="glyphicon glyphicon-plus"></span> Create a new service</h4>
+    </a>
 </div>
 
 <h2>Offline Services:</h2>
-<div class="list-group">
-  <a href="#" class="list-group-item">
+<div class="list-group warning">
+  <a href="#" class="list-group-item list-group-item-danger">
     <h4 class="list-group-item-heading">List group item heading</h4>
     <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
   </a>
-  <a href="#" class="list-group-item">
+  <a href="#" class="list-group-item list-group-item-danger">
     <h4 class="list-group-item-heading">List group item heading</h4>
     <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
   </a>
